6ff05cff1119a566ae976f295b1ca56027a8de3f
firak01 <firak01@t-online.de>
2018-02-01 09:16:16 +0100

    Initiale Erstellung UGAKI-PRV
    Signed-off-by: firak01 <firak01@t-online.de>

---
+++ server/wakeOnLan/wolNarses01FGL.bat
@@ -0,0 +1,7 @@
+REM In das Verzeichnis der .exe wechseln
+cd\
+cd C:\1fgl\server\wakeOnLan
+
+REM IP Adresse /a 192.168.3.110
+REM MAC ADresse
+mc-wol 90:FB:A6:2C:AB:D3 
---
+++ server/wakeOnLan/wolUgakiFGL.bat
@@ -0,0 +1,5 @@
+REM In das Verzeichnis der .exe wechseln
+
+REM IP Adresse /a 192.168.3.110
+REM MAC ADresse
+mc-wol 70:71:BC:CA:63:54 

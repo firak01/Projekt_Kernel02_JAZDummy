71e9d6b40888727eafef90d6013939e985914450
unknown <lindhaueradmin@.(none)>
2018-02-11 13:25:11 +0100

    timesync hinzugenommen, weil die Uhrzeit auf dem Hannibal Rechner immer stehenbleibt. Die Betterie austauschen wäre wohl die andere Alternative...
    Signed-off-by: unknown <lindhaueradmin@.(none)>

--- server/wakeOnLan/wolHannibalFGL.bat
+++ server/wakeOnLan/wolHannibalFGL.bat
@@ -1,11 +1,7 @@
 REM In das Verzeichnis der .exe wechseln
-cd\
-cd C:\1fgl\server\wakeOnLan
 
 REM IP Adresse /a 192.168.3.130
 REM MAC ADresse
-REM alt 20170707 mc-wol 00:11:d8:5f:d1:ff
-
 REM FGL 2011-12-25: Funktioniert nur, wenn der Rechner kurz zuvor an war. Das BIOS scheint die Netzwerkkarte zu deaktivieren nach einiger Zeit.
 REM ??? ipconfig /all gibt aber andere Angaben zurück .... mc-wol 00:11:d8:5f:d1:ff
-mc-wol 00:11:d8:75:92:fe
+mc-wol 00:11:D8:75:92:FE
--- server/wakeOnLan/wolNarses01FGL.bat
+++ server/wakeOnLan/wolNarses01FGL.bat
@@ -1,6 +1,4 @@
 REM In das Verzeichnis der .exe wechseln
-cd\
-cd C:\1fgl\server\wakeOnLan
 
 REM IP Adresse /a 192.168.3.110
 REM MAC ADresse

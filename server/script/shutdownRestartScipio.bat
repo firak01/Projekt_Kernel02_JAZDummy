f8cc132942608724ca50f8f99ab99dedf70ac3a3
firak01 <firak01@t-online.de>
2018-06-12 22:21:02 +0200

    Scipio Signed-off-by: firak01 <firak01@t-online.de>

---
+++ server/script/shutdownRestartScipio.bat
@@ -0,0 +1,5 @@
+cd\
+cd 1fgl\server\script
+start /wait onShutdownScipio.bat
+.\shutdown\shutdown.exe /L /R /T:480 -c "Neustart des Hostsystems. Dringend alle Server runterfahren. Neustart per BIOS Einstellung." -f
+exit
